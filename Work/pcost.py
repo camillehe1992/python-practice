de76c3e10f1a357aa92e3ac8b1647b139d3d3c75
He.Camille <camille.he@outlook.com>
2020-11-14 20:42:11 +0800

    updated pcost.py

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,11 +1,26 @@
 # pcost.py
 #
 # Exercise 1.27
+import sys
+import csv
 
-f = open('Data/portfolio.csv', 'rt')
-headers = next(f)
-cost = 0.0
-for line in f:
-    row = line.split(',')
-    cost = int(row[1]) * float(row[2]) + cost
+def portfolio_cost(filename):
+    try:
+        f = open(filename, 'rt')
+        rows = csv.reader(f)
+        headers = next(rows)
+        cost = 0.0
+        for row in rows:
+            cost = int(row[1]) * float(row[2]) + cost
+        f.close()
+        return cost
+    except FileNotFoundError:
+        print('the file is not exist')
+
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = 'Data/portfolio.csv'
+
+cost = portfolio_cost(filename)
 print('Total cost', cost)
